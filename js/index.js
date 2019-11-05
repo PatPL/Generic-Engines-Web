222c44c6fa5e9ef9e27a238903e838a71dd3e28b
PatPL <patrykwyp@poczta.fm>
2019-11-05 11:04:46 +0100

    points & lines

--- js/index.js
+++ js/index.js
@@ -9480,6 +9480,8 @@ var EngineEditableFieldMetadata;
 })(EngineEditableFieldMetadata || (EngineEditableFieldMetadata = {}));
 var EngineEditableFieldMetadata;
 (function (EngineEditableFieldMetadata) {
+    const chartWidth = 400;
+    const chartHeight = 400;
     EngineEditableFieldMetadata.ThrustCurve = {
         ApplyValueToDisplayElement: (e, engine) => {
             e.innerHTML = engine.ThrustCurve.length > 0 ? `Custom: ${engine.GetThrustCurveBurnTimeMultiplier().toFixed(3)} * Burn time` : "Default";
@@ -9488,44 +9490,101 @@ var EngineEditableFieldMetadata;
             tmp.style.width = "416px";
             tmp.style.height = "500px";
             let chartElement = document.createElement("div");
-            chartElement.style.width = "416px";
-            chartElement.style.height = "417px";
-            chartElement.style.padding = "8px";
-            chartElement.style.boxSizing = "border-box";
-            chartElement.style.borderBottom = "var(--darkBorder) solid 1px";
-            chartElement.style.background = "var(--tableBackground)";
+            chartElement.classList.add("chartElement");
             let chartBackground = document.createElement("canvas");
             chartBackground.classList.add("chartBackground");
-            chartBackground.height = 400;
-            chartBackground.width = 400;
-            chartBackground.style.position = "absolute";
-            chartBackground.style.top = "8px";
-            chartBackground.style.left = "8px";
+            chartBackground.width = chartWidth;
+            chartBackground.height = chartHeight;
             chartElement.appendChild(chartBackground);
             let canvas = chartBackground.getContext("2d");
             let style = getComputedStyle(document.body);
-            CanvasHelper.DrawGrid(0, 0, 399, 399, 9, 3, true, canvas, style.getPropertyValue("--tableRegular"), 1, { 5: { Color: style.getPropertyValue("--tableDistinct"), Label: "50%" } }, { 2: { Color: style.getPropertyValue("--tableRed"), Width: 1, Label: "100%" }, 3: { Color: style.getPropertyValue("--tableDistinct"), Label: "50%" } }, { Color: style.getPropertyValue("--tableBorder"), Width: 3 }, "Fuel", "Thrust");
+            CanvasHelper.DrawGrid(0, 0, chartWidth - 1, chartHeight - 1, 9, 3, true, canvas, style.getPropertyValue("--tableRegular"), 1, { 5: { Color: style.getPropertyValue("--tableDistinct"), Label: "50%" } }, { 2: { Color: style.getPropertyValue("--tableRed"), Width: 1, Label: "100%" }, 3: { Color: style.getPropertyValue("--tableDistinct"), Label: "50%" } }, { Color: style.getPropertyValue("--tableBorder"), Width: 3 }, "Fuel", "Thrust");
             tmp.appendChild(chartElement);
             let chartLines = document.createElement("canvas");
-            chartBackground.classList.add("chartLines");
-            chartLines.height = 400;
-            chartLines.width = 400;
-            chartLines.style.position = "absolute";
-            chartLines.style.top = "8px";
-            chartLines.style.left = "8px";
+            chartLines.classList.add("chartLines");
+            chartLines.height = chartHeight;
+            chartLines.width = chartWidth;
             chartElement.appendChild(chartLines);
             let chartPoints = document.createElement("div");
             chartPoints.classList.add("chartPoints");
-            chartPoints.style.position = "absolute";
-            chartPoints.style.top = "8px";
-            chartPoints.style.left = "8px";
-            chartPoints.style.width = "400px";
-            chartPoints.style.height = "400px";
             chartElement.appendChild(chartPoints);
+            const updateLines = () => {
+                updateLineChart(chartLines.getContext("2d"), getCurve(chartPoints));
+            };
+            chartPoints.addEventListener("dblclick", (e) => {
+                addPoint(chartPoints, e.layerX, e.layerY, updateLines);
+                updateLines();
+            });
+            chartPoints.addEventListener("pointerdown", e => {
+                if (e.which == 1) {
+                    console.log(getCurve(chartPoints));
+                }
+            });
+            updateLines();
             return tmp;
         }, ApplyValueToEditElement: (e, engine) => {
         }, ApplyChangesToValue: (e, engine) => {
         },
+    };
+    const pointRadius = 5;
+    const addPoint = (container, startX, startY, onDrag) => {
+        let newPoint = document.createElement("div");
+        newPoint.classList.add("chartPoint");
+        newPoint.style.left = `${startX - pointRadius}px`;
+        newPoint.style.top = `${startY - pointRadius}px`;
+        container.appendChild(newPoint);
+        newPoint.addEventListener("pointerdown", e => {
+            let startX = Input.MouseX;
+            let startY = Input.MouseY;
+            let originalX = parseInt(newPoint.style.left);
+            let originalY = parseInt(newPoint.style.top);
+            Dragger.Drag(() => {
+                let newX = Math.min(Math.max(originalX + Input.MouseX - startX, -pointRadius), chartWidth - pointRadius);
+                let newY = Math.min(Math.max(originalY + Input.MouseY - startY, -pointRadius), chartHeight - pointRadius);
+                newPoint.style.left = `${newX}px`;
+                newPoint.style.top = `${newY}px`;
+                onDrag();
+            });
+        });
+        newPoint.addEventListener("dblclick", e => {
+            e.preventDefault();
+            e.stopImmediatePropagation();
+        });
+    };
+    function getCurve(pointContainer) {
+        let pointElements = pointContainer.querySelectorAll("div.chartPoint");
+        let points = [];
+        let output = [];
+        pointElements.forEach(e => {
+            points.push([
+                parseInt(e.style.left),
+                parseInt(e.style.top)
+            ]);
+        });
+        points.forEach(([rawFuel, rawThrust]) => {
+            output.push([
+                (rawFuel + pointRadius) / chartWidth,
+                (1 - (rawThrust + pointRadius) / chartHeight) * 2
+            ]);
+        });
+        output = output.sort((a, b) => a[0] - b[0]);
+        return output;
+    }
+    const updateLineChart = (lineChart, points) => {
+        lineChart.clearRect(0, 0, chartWidth, chartHeight);
+        if (points.length == 0) {
+            lineChart.moveTo(0, chartHeight / 2);
+            lineChart.lineTo(chartWidth, chartHeight / 2);
+            return;
+        }
+        lineChart.beginPath();
+        lineChart.moveTo(0, chartHeight - points[0][1] * chartHeight / 2);
+        for (let i = 0; i < points.length; ++i) {
+            lineChart.lineTo(points[i][0] * chartWidth, chartHeight - points[i][1] * chartHeight / 2);
+        }
+        lineChart.lineTo(chartWidth, chartHeight - points[points.length - 1][1] * chartHeight / 2);
+        lineChart.stroke();
+        lineChart.closePath();
     };
 })(EngineEditableFieldMetadata || (EngineEditableFieldMetadata = {}));
 var EngineEditableFieldMetadata;

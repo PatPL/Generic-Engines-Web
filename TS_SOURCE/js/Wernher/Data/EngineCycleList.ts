6884cb60c6b78a73b147163f84d9e5fd8dc3d856
PatPL <patrykwyp@poczta.fm>
2019-12-27 21:32:01 +0100

    Added hydrolox engines to gas generator engine references

--- TS_SOURCE/js/Wernher/Data/EngineCycleList.ts
+++ TS_SOURCE/js/Wernher/Data/EngineCycleList.ts
@@ -23,6 +23,7 @@ const EngineCycleList: { [cycle: number]: IEngineCycle } = { // { [cycle in Engi
                 CanonAvailabilityYear: 1960
             }
         },
+        
         // * * * * References from RP-1 tech tree:
         // * Start:
         // * * RD-100:           307kN, 203s - 237s Alcolox (aux HTP)
@@ -71,6 +72,7 @@ const EngineCycleList: { [cycle: number]: IEngineCycle } = { // { [cycle in Engi
         // * 1963:
         // * * E-1:             1885kN, 260s - 290s Kerolox
         // * * LR79-13:          873kN, 252s - 291s Kerolox
+        // ### LR87-LH2:         667kN, 350s - 403s Hydrolox
         // * 1964:
         // * * RD-855:            83kN, 254s - 292s UDMH+NTO
         // * * H-1-188:          920kN, 263s - 292s Kerolox
@@ -84,10 +86,16 @@ const EngineCycleList: { [cycle: number]: IEngineCycle } = { // { [cycle in Engi
         // * * RD-0110:          298kN, 141s - 330s Kerolox (vacuum trait, minor throttling)
         // * 1966:
         // * * H-1-200:          979kN, 263s - 292s Kerolox
+        // ### J-2:              890kN, 100s - 418s Hydrolox (vacuum trait, ignitions, throttling)
         // * 1967:
         // * * F-1:             7741kN, 263s - 304s Kerolox
         // * * LR105-7.2:        386kN, 220s - 316s Kerolox (sustainer trait)
         // * * LR89-7.2:         951kN, 259s - 293s Kerolox
+        // ### LR87-LH2-Vac:     778kN, 312s - 419s Hydrolox (sustainer trait, ignitions)
+        // ### J-2-225:         1001kN, 100s - 424s Hydrolox (vacuum trait, ignitions, throttling)
+        // * 1968:
+        // ### J-2-230:         1023kN, 100s - 425s Hydrolox (vacuum trait, ignitions, throttling)
+        // ### LR87-LH2+         801kN, 358s - 409s Hydrolox (ignitions)
         // * 1970:
         // * * LR87-11:         1170kN, 254s - 302s Kerolox (sustainer-ish trait)
         // * * LR91-11:          456kN, 200s - 318s Kerolox (vacuum trait)
@@ -96,19 +104,31 @@ const EngineCycleList: { [cycle: number]: IEngineCycle } = { // { [cycle in Engi
         // * * H-1-205:         1018kN, 263s - 292s Kerolox
         // * * LR87-11A:        1211kN, 252s - 304s Kerolox (sustainer-ish trait)
         // * * RS-27:           1023kN, 264s - 295s Kerolox
+        // ### J-2S:            1179kN, 320s - 436s Hydrolox (sustainer trait, ignitions, throttling)
+        // ### LR87-LH2-Vac+     889kN, 233s - 434s Hydrolox (vacuum trait, ignitions)
+        // ### M-1-Spec:        5338kN, 250s - 428s Hydrolox (vacuum trait)
         // * 1976:
         // * * E-1++:           2355kN, 264s - 301s Kerolox
         // * * F-1A:            9190kN, 270s - 310s Kerolox (long burn time trait)
+        // ### HM-7:              62kN, 308s - 440s Hydrolox (vacuum trait)
+        // ### M-1:             6672kN, 250s - 430s Hydrolox (vacuum trait)
+        // ### M-1SL:           6672kN, 310s - 428s Hydrolox (sustainer trait)
         // * 1981:
         // * * LR91-11A:         475kN, 200s - 318s Kerolox (vacuum trait)
+        // ### HM-7B-Early:       64kN, 310s - 442s Hydrolox (vacuum trait)
+        // ### HM-7B:             65kN, 310s - 446s Hydrolox (vacuum trait)
         // * 1986:
         // * * RS-27A:          1054kN, 255s - 302s Kerolox (sustainer-ish trait)
         // * * LR89-RS-56:      1078kN, 262s - 296s Kerolox
         // * * LR105-RS-56:      386kN, 220s - 316s Kerolox (sustainer trait)
+        // ### M-1U:            8007kN, 250s - 430s Hydrolox (vacuum trait)
         // * 1992:
         // * * Vikas-1:          681kN, 248s - 281s UDMH+NTO
+        // ### Vulcain 1:       1113kN, 315s - 439s Hydrolox (Sustainer trait)
         // * 1998:
         // * * S5.92:             20kN, 158s - 327s UDMH+NTO (vacuum trait)
+        // ### RS-68:           3370kN, 357s - 409s Hydrolox (throttling)
+        // ### Vulcain-2:       1359kN, 320s - 429s Hydrolox (sustainer trait)
         // * 2004:
         // * * Merlin-1A:        369kN, 254s - 289s Kerolox
         // * * Vikas-1+:         766kN, 255s - 288s UDMH+NTO
@@ -119,9 +139,11 @@ const EngineCycleList: { [cycle: number]: IEngineCycle } = { // { [cycle in Engi
         // * * Merlin-1D:        722kN, 282s - 311s Kerolox (throttling, ignitions)
         // * * Merlin-1D-Vac:    805kN, 200s - 345s Kerolox (vacuum trait, throttling, ignitions)
         // * * Vikas-2B:         805kN, 261s - 302s UH25+NTO
+        // ### RS-68A:          3570kN, 362s - 412s Hydrolox (throttling) (No clue if it's the correct year)
+        // ### J-2X:            1308kN, 200s - 448s Hydrolox (vacuum trait, minor throttling, ignitions)
         // * 2014:
-        // * * Merlin-1D+:       825kN, 282s - 311s (throttling, ignitions)
-        // * * Merlin-1D++:      914kN, 289s - 311s (throttling, ignitions)
+        // * * Merlin-1D+:       825kN, 282s - 311s Kerolox (throttling, ignitions)
+        // * * Merlin-1D++:      914kN, 289s - 311s Kerolox (throttling, ignitions)
         // * * Merlin-1D-Vac+:   934kN, 200s - 348s Kerolox (vacuum trait, throttling, ignitions)
         // * 2020 (Near future):
         // * * F-1B:            8815kN, 272s - 299s (throttling)
